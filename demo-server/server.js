b3f77e1213555619c3e2f51917de86bc124e661a
EmanuelGF <emanuelfaisca@gmail.com>
2019-08-28 23:02:32 +0100

    Added uuid

--- demo-server/server.js
+++ demo-server/server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3002;
 //middleware
 app.use(bodyParser.json());
 
+app.get('/', (req, res)=> {
+    res.send('home page')
+})
+
+
 
 app.listen(PORT, () =>{
     console.log(`Listening on port: ${PORT}`);
