997d014e79edbdf64c180e9a7cecef5a6bbd256b
EmanuelGF <emanuelfaisca@gmail.com>
2019-11-02 11:34:53 +0000

    refactoring: login component

--- demo-react-ui/src/Components/Login/Login.js
+++ demo-react-ui/src/Components/Login/Login.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import './Login.css';
+import React from 'react'
+import './Login.css'
+import axios from 'axios'
 
-import axios from 'axios';
-
-
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(event){
+export default function Login() {
+    function handleSubmit(event){
         event.preventDefault();
         const email = document.getElementById('email').value;
         const passwd = document.getElementById('passwd').value;
@@ -21,6 +14,10 @@ class Login extends React.Component {
         }).then(response => {
             console.log(response);
             window.location.replace("/"); //redirects the user to the home page
+        }, rejection => {
+            console.log(rejection)
+            alert('An error has ocurred! Please try again.')
+            window.location.replace("/login");
         }).catch(error=>{
             console.log(error);
             alert('An error has ocurred! Please try again.')
@@ -28,27 +25,22 @@ class Login extends React.Component {
         })
     }
 
-    render(){
-        return (
-            <div id="login" className="row justify-content-center">
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
-        
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="passwd" placeholder="Password"/>
-                    </div>
-      
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        );
-
-
-    }
+    return (
+        <div id="login" className="row justify-content-center">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
+    
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="passwd" placeholder="Password"/>
+                </div>
+    
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );    
 }
 
-export default Login;
