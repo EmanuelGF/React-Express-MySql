ca09dddf7b155b850c04998d5e6026ceede14382
EmanuelGF <emanuelfaisca@gmail.com>
2019-11-02 11:30:36 +0000

    REFACTORING app.js

--- demo-react-ui/src/App.js
+++ demo-react-ui/src/App.js
@@ -1,68 +1,55 @@
-import React from 'react';
-import './App.css';
-import Nav from './Components/Nav/Nav';
-import Login from './Components/Login/Login';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import Nav from './Components/Nav/Nav'
+import Login from './Components/Login/Login'
+import axios from 'axios'
 import { 
   BrowserRouter,
   Route
-} from 'react-router-dom';
+} from 'react-router-dom'
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isUserLogged: 'false',
-      username: ''
-    }
-  }
+ export default function App() {
+  const [isUserLogged, setIsUserLogged] = useState(false)
+  const [username, setUsername] = useState('')
 
   //This is used to get the user status and pass user data around.
-  checkLoginStatus() {
+  function checkLoginStatus() {
     axios.get('/logged_in')
     .then(response=> {
       console.log(response)
-      if(response.data.logged_in && this.state.isUserLogged === 'false') {
-        this.setState({
-          isUserLogged: 'true',
-          username: response.data.username
-        })
-      } else if(!response.data.logged_in && this.state.isUserLogged === 'true') {
-        this.setState({
-          isUserLogged: '',
-          username: ''
-        })
+      if(response.data.logged_in && isUserLogged === 'false') {
+        setIsUserLogged(true)
+        setUsername(response.data.username)
+      } else if(!response.data.logged_in && isUserLogged === 'true') {
+        setIsUserLogged('')
+        setUsername('')
       }
     })
   }
 
-  componentDidMount() {
-    this.checkLoginStatus();
-  }
+  useEffect(()=>{
+    checkLoginStatus();
+  })
 
-  render(){
-    return (
-      <div className="App container">
-        <BrowserRouter>
-          <Nav isUserLogged={this.state.isUserLogged} username={this.state.username}/>   
-          <div className="row">
-          <div className="col-md-12 text-center">
-            <br />
-            <h3>Welcome to the demo app for REACT EXPRESS MYSQL with AUTH(sessions and passport) </h3>
-          </div>
-          <div className="col-md-3">
-            <ul>
-              <li>Login with:</li>
-              <li><b>email: </b>test@test.com</li>
-              <li><b>password: </b>test</li>
-            </ul>
-          </div>
-        </div> 
-          <Route path="/login" component={Login} exact />   
-        </BrowserRouter>
-      </div>
-    );
-  }  
+  return (
+    <div className="App container">
+      <BrowserRouter>
+        <Nav isUserLogged={isUserLogged} username={username}/>   
+        <div className="row">
+        <div className="col-md-12 text-center">
+          <br />
+          <h3>Welcome to the demo app for REACT EXPRESS MYSQL with AUTH(sessions and passport) </h3>
+        </div>
+        <div className="col-md-3">
+          <ul>
+            <li>Login with:</li>
+            <li><b>email: </b>test@test.com</li>
+            <li><b>password: </b>test</li>
+          </ul>
+        </div>
+      </div> 
+        <Route path="/login" component={Login} exact />   
+      </BrowserRouter>
+    </div>
+  );
 }
-  
-export default App;
